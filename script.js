5cddfe00ef3677c34b313791beab875e88cf9cdc
JamesRileydev <jamesriley.dev@gmail.com>
2018-06-30 18:01:31 -0400

    Added js for nav links to uncheck nav bar

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 for (var i = 0; i < 30; i++) {
-  var star = '<div class="star" style="animation: twinkle ' + ((Math.random() * 5) + 3) + 's linear ' + 
-  1 + 's infinite; top: ' + Math.random() * 250 + 'px; left: ' 
-  + Math.random() * 800 + 'px;"></div>';
-  console.log(star);
+  var star = '<div class="star" style="animation: twinkle ' + ((Math.random() * 5) + 3) + 's linear ' +
+    1 + 's infinite; top: ' + Math.random() * 250 + 'px; left: '
+    + Math.random() * 800 + 'px;"></div>';
+  // console.log(star);
   $('.landing-body').append(star);
   // $('#star-container').css('backgroundColor', 'white');
 }
+
+
+$('.navigation__link').click(function () {
+  $(".navigation__checkbox").prop("checked", false);
+  });
