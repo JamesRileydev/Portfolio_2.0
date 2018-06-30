fecb2624ab1670a9a8e34a7412826fe36ce4540f
JamesRileydev <jamesriley.dev@gmail.com>
2018-06-30 18:11:38 -0400

    Adjusted star color

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-for (var i = 0; i < 30; i++) {
+for (var i = 0; i < 20; i++) {
   var star = '<div class="star" style="animation: twinkle ' + ((Math.random() * 5) + 3) + 's linear ' +
     1 + 's infinite; top: ' + Math.random() * 250 + 'px; left: '
     + Math.random() * 800 + 'px;"></div>';
