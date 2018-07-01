51a96180688a77d15141adc12ad3dd3cbb18c5f8
JamesRileydev <jamesriley.dev@gmail.com>
2018-07-01 10:05:05 -0400

    Still messin with the stars

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-for (var i = 0; i < 20; i++) {
+for (var i = 0; i < 100; i++) {
   var star = '<div class="star" style="animation: twinkle ' + ((Math.random() * 5) + 3) + 's linear ' +
     1 + 's infinite; top: ' + Math.random() * 250 + 'px; left: '
-    + Math.random() * 800 + 'px;"></div>';
+    + Math.random() * 850 + 'px;"></div>';
   // console.log(star);
   $('.landing-body').append(star);
   // $('#star-container').css('backgroundColor', 'white');
